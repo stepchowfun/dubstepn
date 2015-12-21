975e26e4227a4ca4d4dce25c6292de39295a9edf
Stephan Boyer <stephan@stephanboyer.com>
2015-12-20 18:02:48 -1000

    Fix routes and some styles

--- config/routes.rb
+++ config/routes.rb
@@ -55,41 +55,41 @@ Rails.application.routes.draw do
   #   end
 
   # public routes
-  root "home#posts_for_tag"
+  root "home#posts_for_tag", :format => false
   get "/post/:post_id/*title" => "home#post", :format => false
   get "/post/:post_id" => "home#post", :format => false
-  get "/resume" => "home#resume"
-  get "/resume.pdf" => "home#resume"
+  get "/about" => "home#about", :format => false
+  get "/resume" => "home#resume", :format => false
 
   # for web crawlers and search engines
-  get "/robots.txt" => "home#robots"
-  get "/sitemap" => "home#sitemap"
+  get "/robots.txt" => "home#robots", :format => false
+  get "/sitemap" => "home#sitemap", :format => false
 
   # syndication
-  get "/rss/home" => redirect("/rss")
-  get "/rss" => "home#feed", :type => :rss
-  get "/rss/:tag" => "home#feed", :type => :rss, :format => false
-  get "/atom/home" => redirect("/atom")
-  get "/atom" => "home#feed", :type => :atom
-  get "/atom/:tag" => "home#feed", :type => :atom, :format => false
+  get "/rss/home" => redirect("/rss"), :format => false
+  get "/rss" => "home#feed", :format => false
+  get "/rss/:tag" => "home#feed", :format => false
+  get "/atom/home" => redirect("/atom"), :format => false
+  get "/atom" => "home#feed", :format => false
+  get "/atom/:tag" => "home#feed", :format => false
 
   # admin panel
-  get "/admin/index" => "home#admin"
+  get "/admin/index" => "home#admin", :format => false
   get "/admin/edit_post/:post_id" => "home#edit_post", :format => false
-  post "/admin/create_post" => "home#create_post_action"
-  post "/admin/move_up" => "home#move_up_action"
-  post "/admin/move_down" => "home#move_down_action"
-  post "/admin/move_top" => "home#move_top_action"
-  post "/admin/move_bottom" => "home#move_bottom_action"
-  post "/admin/edit_post" => "home#edit_post_action"
-  post "/admin/delete_post" => "home#delete_post_action"
-  post "/admin/create_redirect" => "home#create_redirect_action"
-  post "/admin/delete_redirect" => "home#delete_redirect_action"
+  post "/admin/create_post" => "home#create_post_action", :format => false
+  post "/admin/move_up" => "home#move_up_action", :format => false
+  post "/admin/move_down" => "home#move_down_action", :format => false
+  post "/admin/move_top" => "home#move_top_action", :format => false
+  post "/admin/move_bottom" => "home#move_bottom_action", :format => false
+  post "/admin/edit_post" => "home#edit_post_action", :format => false
+  post "/admin/delete_post" => "home#delete_post_action", :format => false
+  post "/admin/create_redirect" => "home#create_redirect_action", :format => false
+  post "/admin/delete_redirect" => "home#delete_redirect_action", :format => false
 
   # authentication
-  get "/admin/login" => "home#login"
-  post "/admin/login" => "home#login_action"
-  post "/admin/logout" => "home#logout_action"
+  get "/admin/login" => "home#login", :format => false
+  post "/admin/login" => "home#login_action", :format => false
+  post "/admin/logout" => "home#logout_action", :format => false
 
   # tags
   get "/home" => redirect("/")
